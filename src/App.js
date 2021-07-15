96c1672adfe6befaba3c1c25f79e754faff0c2df
username <euny_sky11@naver.com>
2021-07-15 17:06:39 +0900

    semi-final

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {HashRouter, Route} from "react-router-dom";
 import About from "./routes/about";
 import Home from "./routes/home";
+import Navi from "./components/navi";
+import Detail from "./routes/detail";
 
 function App(){
   return (
   <HashRouter>
+    <Navi/>
     <Route path="/" exact={true} component={Home}/>
     <Route path="/about" exact={true} component={About}/>
+    <Route path="/movie/:id" component={Detail} />
   </HashRouter>
   );
 }
--- src/components/movie.js
+++ src/components/movie.js
@@ -1,35 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import './movie.css';
+import "./movie.css";
 
-function Movie({id, year, title, summary, poster, genres}){
-    return (
-    <div className="movies_movie">
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+      >
         <img src={poster} alt={title} title={title} />
-        <div className="movie_data">
-            <h3 className="movie_title">{title}</h3>
-            <h5 className="movie_year">{year}</h5>
-            <ul className="movie_genres">
-                {genres.map((genre, index) => 
-                <li key={index} className="movie__genre">
-                    {genre}
-                </li>)}
-            </ul>
-            <p className="movie_summary">{summary}</p>
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
-
+      </Link>
     </div>
-    )
+  );
 }
 
-
 Movie.propTypes = {
-    id : PropTypes.number.isRequired,
-    year : PropTypes.number.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired,
-    genres : PropTypes.arrayOf(PropTypes.string).isRequired
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
